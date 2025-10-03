49fda34bf09678d03371674c4ea145d216b3d8cb
Redinfernowith <108304880+stri4759@users.noreply.github.com>
2025-10-02 17:52:13 -0700

    new

--- club-directory-api/api/constants.py
+++
@@ -1,7 +0,0 @@
-SHEET_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSpuDZTPBnKX6fPmFJDikNbXhul71FCf0sg7sOs5zOd47lCO3Y5ERLTSS-FtQBXyJa25ZTFvMLoR5-h/pub?gid=0&single=true&output=csv"
-CLUB_SHEET_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSpuDZTPBnKX6fPmFJDikNbXhul71FCf0sg7sOs5zOd47lCO3Y5ERLTSS-FtQBXyJa25ZTFvMLoR5-h/pub?gid=658017674&single=true&output=csv"
-CLASS_CAT_SHORTNAMES = ["ssci", "engl", "math", "sci", "flang", "vapa", "physed", "elect", "cllg"]
-
-WEIGHTED_DESC = "Course is given an extra grade point in GPA if the course is completed with a \"C\" or higher"
-THIRD_YEAR_DESC = 'Courses in this category fulfill the FCUSD graduation requirement of "Fine Art/Foreign Language/CTE" or "3rd year science/CTE"'
-AG_DESC = 'Courses in this category have been approved by the University of California as College Prep courses that are used to calculate college eligibility'
